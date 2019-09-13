73a794cc643f77e88edf02d366f3e7300dc165b8
Lucas Leite <me@lucasleite.us>
2019-09-13 11:59:49 -0300

    Update templates folder structure

--- app.py
+++ app.py
@@ -4,42 +4,42 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("admin/index.html")
 
 
 @app.route("/finances")
 def finances():
-    return render_template("finances.html")
+    return render_template("admin/finances.html")
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("admin/login.html")
 
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("admin/register.html")
 
 
 @app.route("/students")
 def students():
-    return render_template("students.html")
+    return render_template("admin/students.html")
 
 
 @app.route("/lectures")
 def lectures():
-    return render_template("lectures.html")
+    return render_template("admin/lectures.html")
 
 
 @app.route("/settings/lectures")
 def settings_lectures():
-    return render_template("settings/lectures.html")
+    return render_template("admin/settings/lectures.html")
 
 
 @app.route("/settings/student-evaluation")
 def settings_student_evaluation():
-    return render_template("settings/student-evaluation.html")
+    return render_template("admin/settings/student-evaluation.html")
 
 
 if __name__ == "__main__":
