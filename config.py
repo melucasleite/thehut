f19f9e38b35753fa1eb89f32f66a5dba9538f457
Lucas Leite <me@lucasleite.us>
2019-09-24 19:59:29 -0300

    Add bucket requirements

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
-DEBUG = True
 import os
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", 'mysql+pymysql://root:root@localhost:3306/thehut')
+from dotenv import load_dotenv
+load_dotenv()
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "SQLALCHEMY_DATABASE_URI", 'mysql+pymysql://root:root@localhost:3306/thehut')
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'thehut'
+S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
+S3_KEY = os.environ.get("S3_ACCESS_KEY")
+S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
