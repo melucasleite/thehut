cdb9edf97f374958cdb60eb8431047cc04bd2541
Lucas Leite <me@lucasleite.us>
2019-09-26 23:55:36 -0300

    remove s3

--- config.py
+++ config.py
@@ -7,7 +7,3 @@ SQLALCHEMY_DATABASE_URI = os.environ.get(
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'thehut'
-S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
-S3_KEY = os.environ.get("S3_ACCESS_KEY")
-S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
-S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
