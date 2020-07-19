daf28004b408a837164d53e70a09c0c305e91c94
lucas leite <me@lucasleite.us>
2020-07-19 14:52:06 -0400

    Fix dockerfile

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = os.environ.get(
-    "SQLALCHEMY_DATABASE_URI", 'mysql+pymysql://root:root@localhost:3306/thehut')
+    "SQLALCHEMY_DATABASE_URI", 'mysql+pymysql://root:thehut@localhost:3306/thehut')
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'thehut'
