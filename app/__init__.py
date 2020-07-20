cdd9529132787314dafb8ceb0b1e13519fb10158
lucas leite <me@lucasleite.us>
2020-07-19 21:42:16 -0400

    store jobs in database

--- app/__init__.py
+++ app/__init__.py
@@ -34,11 +34,13 @@ from app.jobs.lectureHistory import generate_lecture_history
 from app.jobs.studentPayment import generate_payments
 
 scheduler = BackgroundScheduler(
-    jobstores={'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')})
+    jobstores={'default': SQLAlchemyJobStore(url=os.environ.get(
+        "SQLALCHEMY_DATABASE_URI", 'sqlite:///jobs.sqlite'
+    ))})
 scheduler.start()
 scheduler.add_job(generate_lecture_history, id="generate_lecture_history", replace_existing=True, trigger="interval",
                   minutes=5)
-scheduler.add_job(generate_payments, id="generate_payments", replace_existing=True, trigger="interval", minutes=1)
+scheduler.add_job(generate_payments, id="generate_payments", replace_existing=True, trigger="interval", days=1)
 
 if __name__ == "__main__":
     app.run(debug=True)
