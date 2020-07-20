75aeef346824e276bcdb0fa23e1718120d9146ab
lucas leite <me@lucasleite.us>
2020-07-19 21:31:54 -0400

    Add sentry

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,25 @@
-#encoding=utf-8
-from flask import Flask, render_template, redirect, url_for, jsonify
+# encoding=utf-8
+import os
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import login_required
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import logging
+
 logging.basicConfig()
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+os.environ.get("SENTRY_URI")
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_URI"),
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -24,8 +36,14 @@ from app.jobs.studentPayment import generate_payments
 scheduler = BackgroundScheduler(
     jobstores={'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')})
 scheduler.start()
-scheduler.add_job(generate_lecture_history, id="generate_lecture_history", replace_existing=True, trigger="interval", minutes=5)
+scheduler.add_job(generate_lecture_history, id="generate_lecture_history", replace_existing=True, trigger="interval",
+                  minutes=5)
 scheduler.add_job(generate_payments, id="generate_payments", replace_existing=True, trigger="interval", minutes=1)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
